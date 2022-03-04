2c3bcefa182c998657528a5ed24d9b519032f0e5
Eric Hunter <ehunter7@live.com>
2022-03-03 21:11:27 -0800

    add pull to refresh

--- Groceries/app/screens/MessagesScreen.jsx
+++ Groceries/app/screens/MessagesScreen.jsx
@@ -23,6 +23,7 @@ const initialMessages = [
 
 function MessagesScreen(props) {
   const [messages, setMessages] = useState(initialMessages)
+  const [refreshing, setRefreshing] = useState(false)
 
   const handleDelete = (message) => {
     setMessages(messages.filter((m) => m.id !== message.id))
@@ -45,6 +46,17 @@ function MessagesScreen(props) {
           />
         )}
         ItemSeparatorComponent={ListItemSeperator}
+        refreshing={refreshing}
+        onRefresh={() =>
+          setMessages([
+            {
+              id: 1,
+              title: 'T1',
+              description: 'D1',
+              image: require('../assets/lenai.png'),
+            },
+          ])
+        }
       />
     </Screen>
   )
