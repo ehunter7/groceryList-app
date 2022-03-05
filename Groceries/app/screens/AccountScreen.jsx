e3960860d53fd008b2b31ae9f84ea9da7e5e69af
Eric Hunter <ehunter7@live.com>
2022-03-05 08:17:57 -0800

    add account screen

---
+++ Groceries/app/screens/AccountScreen.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { FlatList, StyleSheet, View } from 'react-native'
+import Icon from '../components/Icon'
+import ListItem from '../components/ListItem'
+import ListItemSeperator from '../components/ListItemSeperator'
+import Screen from '../components/Screen'
+import colors from '../config/colors'
+
+const menuItems = [
+  {
+    title: 'My Recipes',
+    icon: {
+      name: 'format-list-bulleted',
+      backgroundColor: colors.primary,
+    },
+  },
+  {
+    title: 'My Messages',
+    icon: {
+      name: 'email',
+      backgroundColor: colors.secondary,
+    },
+  },
+]
+
+function AccountScreen(props) {
+  return (
+    <Screen style={styles.screen}>
+      <View style={styles.container}>
+        <ListItem
+          title="Lenai"
+          subTitle="#1 Chef"
+          image={require('../assets/lenai.png')}
+        />
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(item) => item.title}
+          ItemSeparatorComponent={ListItemSeperator}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.title}
+              IconComponent={
+                <Icon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
+            />
+          )}
+        />
+      </View>
+      <ListItem
+        title="Log Out"
+        IconComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
+      />
+    </Screen>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 20,
+  },
+  screen: {
+    backgroundColor: colors.light,
+  },
+})
+
+export default AccountScreen
