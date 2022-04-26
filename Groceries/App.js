192b0a9e135b955940dd215a0857e1c9a13166e2
Eric Hunter <ehunter7@live.com>
2022-04-25 18:13:35 -0700

    add async storage

--- Groceries/App.js
+++ Groceries/App.js
@@ -43,61 +43,78 @@ import Screen from "./app/components/Screen";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Tweets = ({ navigation }) => (
-  <Screen>
-    <Text>Tweets</Text>
-    <Button
-      title="View Tweet"
-      onPress={() => navigation.navigate("TweetDetails")}
-    />
-  </Screen>
-);
+// const Tweets = ({ navigation }) => (
+//   <Screen>
+//     <Text>Tweets</Text>
+//     <Button
+//       title="View Tweet"
+//       onPress={() => navigation.navigate("TweetDetails")}
+//     />
+//   </Screen>
+// );
 
-const TweetDetails = () => (
-  <Screen>
-    <Text>Tweet Details</Text>
-  </Screen>
-);
+// const TweetDetails = () => (
+//   <Screen>
+//     <Text>Tweet Details</Text>
+//   </Screen>
+// );
 
-const Stack = createNativeStackNavigator();
-const StackNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Tweets" component={Tweets} />
-    <Stack.Screen name="TweetDetails" component={TweetDetails} />
-  </Stack.Navigator>
-);
+// const Stack = createNativeStackNavigator();
+// const StackNavigator = () => (
+//   <Stack.Navigator>
+//     <Stack.Screen name="Tweets" component={Tweets} />
+//     <Stack.Screen name="TweetDetails" component={TweetDetails} />
+//   </Stack.Navigator>
+// );
 
-const Account = () => (
-  <Screen>
-    <Text>Account</Text>
-  </Screen>
-);
+// const Account = () => (
+//   <Screen>
+//     <Text>Account</Text>
+//   </Screen>
+// );
 
-const Tab = createBottomTabNavigator();
-const TabNavigator = () => (
-  <Tab.Navigator
-    screenOptions={{
-      tabBarActiveBackgroundColor: "tomato",
-      tabBarActiveTintColor: "white",
-      tabBarInactiveBackgroundColor: "#eee",
-      tabBarInactiveTintColor: "black",
-    }}
-  >
-    <Tab.Screen
-      name="Feed"
-      component={StackNavigator}
-      options={{
-        tabBarIcon: ({ size, color }) => (
-          <MaterialCommunityIcons name="home" size={size} color={color} />
-        ),
-      }}
-    />
-    <Tab.Screen name="Account" component={Account} />
-  </Tab.Navigator>
-);
+// const Tab = createBottomTabNavigator();
+// const TabNavigator = () => (
+//   <Tab.Navigator
+//     screenOptions={{
+//       tabBarActiveBackgroundColor: "tomato",
+//       tabBarActiveTintColor: "white",
+//       tabBarInactiveBackgroundColor: "#eee",
+//       tabBarInactiveTintColor: "black",
+//     }}
+//   >
+//     <Tab.Screen
+//       name="Feed"
+//       component={StackNavigator}
+//       options={{
+//         tabBarIcon: ({ size, color }) => (
+//           <MaterialCommunityIcons name="home" size={size} color={color} />
+//         ),
+//       }}
+//     />
+//     <Tab.Screen name="Account" component={Account} />
+//   </Tab.Navigator>
+// );
 
 export default function App() {
+  const netinfo = useNetInfo();
+
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
+
+      const value = await AsyncStorage.getItem("person");
+      const person = JSON.parse(value);
+      console.log(person);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  demo();
+
   return (
     <NavigationContainer theme={navigationTheme}>
       <AppNavigator />
