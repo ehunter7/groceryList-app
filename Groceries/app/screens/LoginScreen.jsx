73dc1ce8d04cb4638af71ae921145ea2086855fa
Eric Hunter <ehunter7@live.com>
2022-06-02 12:17:17 -0700

    add new password min

--- Groceries/app/screens/LoginScreen.jsx
+++ Groceries/app/screens/LoginScreen.jsx
@@ -15,7 +15,7 @@ import api from '../api/api'
 
 const validationSchemas = Yup.object().shape({
   email: Yup.string().required().email().label('Email'),
-  password: Yup.string().required().min(4).label('Password'),
+  password: Yup.string().required().min(6).label('Password'),
 })
 
 function LoginScreen() {
