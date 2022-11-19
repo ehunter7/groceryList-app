be4eb77c63ac17400c8ef1d82dfa00726602a939
Eric Hunter <ehunter7@live.com>
2022-11-18 18:27:17 -0800

    add a bunch of stuff

--- Groceries/app/screens/RecipeDetailsScreen.jsx
+++ Groceries/app/screens/RecipeDetailsScreen.jsx
@@ -14,9 +14,18 @@ import IngredientsList from '../components/IngredientsList'
 import ListItem from '../components/ListItem'
 import RecipeInstructions from '../components/RecipeInstructions'
 import colors from '../config/colors'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 function RecipeDetailsScreen({ route }) {
   const listing = route.params.recipe
+  let prep = 0
+  let cook = 0
+
+  if (listing.preTime || listing.cookTime) {
+    prep = parseInt(listing.prepTime)
+    cook = parseInt(listing.cookTime)
+  }
+  const totalTime = prep + cook
   return (
     <ScrollView>
       <View style={styles.container}>
@@ -24,6 +33,39 @@ function RecipeDetailsScreen({ route }) {
         <View style={styles.detailsContainer}>
           <AppText style={styles.title}>{listing.title}</AppText>
           <AppText style={styles.description}>{listing.description}</AppText>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            marginBottom: 15,
+          }}
+        >
+          <View style={{ alignItems: 'center' }}>
+            <MaterialCommunityIcons
+              name="clock"
+              size={20}
+              color={colors.medium}
+            />
+            <Text>Prep {prep} mins</Text>
+          </View>
+          <View style={{ alignItems: 'center' }}>
+            <MaterialCommunityIcons
+              name="clock"
+              size={20}
+              color={colors.medium}
+            />
+            <Text>Cook {cook} mins</Text>
+          </View>
+          <View style={{ alignItems: 'center' }}>
+            <MaterialCommunityIcons
+              name="clock"
+              size={20}
+              color={colors.medium}
+            />
+
+            <Text>Total {totalTime} mins</Text>
+          </View>
         </View>
         <IngredientsList ingredients={listing.ingredients} />
         <RecipeInstructions instructions={listing.instructions} />
