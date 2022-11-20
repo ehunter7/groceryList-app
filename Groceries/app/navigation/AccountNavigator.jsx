521d6052270fbb928d291d9df413e95c5a300f3d
Eric Hunter <ehunter7@live.com>
2022-11-19 21:48:56 -0800

    Added recipes to account nav

--- Groceries/app/navigation/AccountNavigator.jsx
+++ Groceries/app/navigation/AccountNavigator.jsx
@@ -4,14 +4,16 @@ import AccountScreen from '../screens/AccountScreen'
 import MessagesScreen from '../screens/MessagesScreen'
 import route from './route'
 import FamilyScreen from '../screens/FamilyScreen'
+import RecipesScreen from '../screens/RecipesScreen'
 
 const Stack = createNativeStackNavigator()
 
 const AcccountNavigator = () => (
-  <Stack.Navigator mode="modal" screenOptions={{ headerShown: false }}>
+  <Stack.Navigator mode="modal" screenOptions={{ headerShown: true }}>
     <Stack.Screen name={route.ACCOUNT} component={AccountScreen} />
     <Stack.Screen name={route.MESSAGES} component={MessagesScreen} />
     <Stack.Screen name={route.FAMILY} component={FamilyScreen} />
+    <Stack.Screen name={route.RECIPES} component={RecipesScreen} />
   </Stack.Navigator>
 )
 
